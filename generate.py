07e8a17fae16df1ce22a03c50b0069a506aa9cc7
paulden <paul.de-nonancourt@student.ecp.fr>
2017-01-04 19:29:54 +0100

    Added functions to convert grids to bytes

---
+++ generate.py
@@ -0,0 +1,47 @@
+from random import randint
+
+
+def generate_grid(n):
+    """Generates a random grid"""
+    # generate random connections, place 0 at the border
+    grid = [0]*n
+    lr = [[randint(0, 1) for j in range(n-1)] + [0] for i in range(n)]
+    tb = [[randint(0, 1) for j in range(n)]  for i in range(n-1)]
+    tb += [[0] * n]
+    for i in range(n):
+        grid[i] = [0]*n
+        for j in range(n):
+            # encoding of the cell
+            c = tb[i - 1][j] + 2*lr[i][j - 1] + 4*tb[i][j] + 8*lr[i][j]
+            cc = c | (c << 4)
+            # normalized orientation
+            p = min((cc >> i) & 15 for i in range(4))
+            grid[i][j] = int(hex(p)[-1], 16)
+    return grid
+
+
+def grid_to_byte(grid):
+    """Converts a grid in its byte equivalent
+    for each square"""
+    n = len(grid)
+    byte_grid = [0]*n
+    for i in range(n):
+        byte_grid[i] = [0]*n
+        for j in range(n):
+            byte_grid[i][j] = [0] * 4
+            b = list(map(int, bin(grid[i][j])[2:]))
+            for k in range(len(b)):
+                byte_grid[i][j][k] = b[k]
+    return byte_grid
+
+
+def byte_to_grid(byte_grid):
+    """Opposite function of grid_to_byte"""
+    n = len(byte_grid)
+    grid = [0]*n
+    for i in range(n):
+        grid[i] = [0]*n
+        for j in range(n):
+            for k in range(4):
+                grid[i][j] += byte_grid[i][j][k]*(2**k)
+    return grid
